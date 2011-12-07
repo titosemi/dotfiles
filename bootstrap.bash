00594fd873802a5b7d8894f8effd7de11cb7e245
Josemi Liébana <office@josemi-liebana.com>
2011-12-07 08:36:33 +0100

    Added git init and remote repo url for pulling :)
    Signed-off-by: Josemi Liébana <office@josemi-liebana.com>

--- bootstrap.bash
+++ bootstrap.bash
@@ -28,12 +28,16 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     # Get the name of the current script
     SCRIPT=`basename $0`;
 
-    # If we are in a git repo. Update the files
-    if [ $(git rev-parse --is-inside-work-tree) = "true" ]; then
-        MSG="Attempting to update repository..."  
-        OUTPUT=$(git pull origin master 2>&1)       
-        output
+    # If we are in a not in a git repo, initialiate it and update the files
+    if [ $(git rev-parse --is-inside-work-tree) != "true" ]; then
+        $(git init 2>&1)
+        $(git remote add origin git@github.com:titosemi/dotfiles.git 2>&1)
     fi
+        
+    MSG="Attempting to update repository..."  
+    OUTPUT=$(git pull origin master 2>&1)       
+    output
+
     
     # Copy the files
     for file in $(ls -la |grep ^- | awk '{print ($9)}'); do
